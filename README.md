59459233598eae9640604b29b457019128216360
Dajne Win <2647116+cptwin@users.noreply.github.com>
2023-07-11 11:34:24 +1200

    First
    v0.1

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
-# Efflanrs
-Efflanrs - GUI for Snaffler Output
+# Efflanrs - Snaffler GUI
+```
+███████ ███████ ███████ ██       █████  ███    ██ ██████  ███████
+██      ██      ██      ██      ██   ██ ████   ██ ██   ██ ██
+█████   █████   █████   ██      ███████ ██ ██  ██ ██████  ███████
+██      ██      ██      ██      ██   ██ ██  ██ ██ ██   ██      ██
+███████ ██      ██      ███████ ██   ██ ██   ████ ██   ██ ███████
+```
+
+## What is it for?
+
+Efflanrs is a Python script to turn your Snaffler "delicious candy needles" into a nice searchable and sortable interface.
+TL:DR it is a GUI for Snaffler output.
+
+[Snaffler](https://github.com/SnaffCon/Snaffler)
+"Snaffler is a tool for **pentesters** and **red teamers** to help find delicious candy needles (creds mostly, but it's flexible) in a bunch of horrible boring haystacks (a massive Windows/AD environment)."
+
+## Features
+- Parse Snaffler JSON
+- Parse Snaffler Standard Out
+- Output to a (nice-ish) HTML view that can be searched through, sorted, or filtered.
+
+
+## Screenshots
+![Example Efflanrs screenshot](./EfflanrsExampleScreenshot.png)
+
+## Setup
+
+Python 3
+```
+pip install -r requirements.txt
+```
+
+## Usage
+
+Usage is simple, provide a Snaffler Standard output log or JSON.
+
+```
+python3 snaffmap.py snaffler.output
+```
+
+```
+python3 snaffmap.py snaffler.json
+```
+
+```
+python3 snaffmap.py "./example data/snaffler.json"
+.,:::::: .-:::::'.-:::::' :::      :::.    ::.    :::.  :::::::.. .::::::.:
+;;;;'''' ;;;'''' ;;;''''  ;;;      ;;`;;   ;;;;,  `;;; ;;;;``;;;;;;;`    ``
+ [[cccc  [[[,,== [[[,,==  [[[     ,[[ '[[,  [[[[[. '[[  [[[,/[[[''[==/[[[[,
+ $$      `$$$'`` `$$$'``  $$'    c$$$cc$$$c $$$ 'Y$c$$   $$$$$$c   '''    $
+888oo,__  888     888   o88oo,.__888   888,888    Y88  888b '88bo,88b    dP
+M''''YUM   'MM,    'MM,  ''''YUMM YMM   ''` MMM     YMMMMMMM   'W'  'YMmMY'
+###############################################
+Great, looks like you specified a JSON file ...
+Here we go!
+###############################################
+ * Serving Flask app 'snaffmap'
+ * Debug mode: off
+WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
+ * Running on http://127.0.0.1:5000
+```
+
+## Project Status
+Project is: _always in progress_
+
+
+## TODO
+
+- Add a button to export to static HTML
+
+## Acknowledgements
+Credit goes here:
+- This project was inspired by an internal tool idea snaffmap
+- Many thanks to [Snaffler](https://github.com/SnaffCon/Snaffler), keep on Snaffling
+- Feabell for the original Flask templating
+
+## Contact
+
+[CptWin](https://github.com/cptwin)
+
+## License
+
+GNU GPL Version 3
+
+¯_(ツ)_/¯
+
+I just copied the Snaffler one
---
+++ snaffmap.py
@@ -0,0 +1,112 @@
+##imports
+import json
+import os
+import sys
+from flask import (Flask, render_template)
+import re
+import webbrowser
+
+##CLASSES
+class Finding:
+    def __init__(self, rating, full_name, creation_time, last_write_time):
+        self.rating = rating
+        self.full_name = full_name
+        self.creation_time = creation_time
+        self.last_write_time = last_write_time
+
+    def __str__(self) -> str:
+        return self.fullPath
+
+app = Flask(__name__)
+
+### Globals
+global filename
+global findings
+
+@app.route("/")
+def index():
+    return render_template("index.html", findings=findings)
+                
+##iterate the JSON once(this is computationally expensive to do twice but it will stop the error of a finding not having an identified share)
+def parseSnafflerJSON(inJsonFile):
+    findings = []
+    for jsonObject in inJsonFile['entries']:
+        if jsonObject['level'] == "Warn":
+            for eventProperty in jsonObject['eventProperties'].keys():
+                for fileProperties in jsonObject['eventProperties'][eventProperty].keys():
+                    for moreProperties in jsonObject['eventProperties'][eventProperty][fileProperties]:
+                        if moreProperties == "MatchedRule":
+                            rating = jsonObject['eventProperties'][eventProperty][fileProperties]['MatchedRule']['Triage']
+                            creation_time = jsonObject['eventProperties'][eventProperty]['FileResult']['FileInfo']['CreationTime']
+                            last_write_time = jsonObject['eventProperties'][eventProperty]['FileResult']['FileInfo']['LastWriteTime']
+                            full_name = jsonObject['eventProperties'][eventProperty]['FileResult']['FileInfo']['FullName']
+                            findings.append(Finding(rating, full_name, creation_time, last_write_time))
+    return findings
+
+def populateSharesfromJSON(inJSONFile):
+    json_string = ""
+    for line in open(inJSONFile, "r"):
+        json_string += line
+    jsonFileObject = json.loads(json_string)
+    return parseSnafflerJSON(jsonFileObject)
+
+def populateSharesfromTXT(inTxtFile):
+    findings = []
+    fileLineTest = re.compile(r"(\[File\])")
+    for line in open(inTxtFile, "r"):
+        if fileLineTest.search(line):
+            rating = re.search(r"(?<=\{)(.*?)(?=\})", line).group(1)
+            full_name = re.search(r"(?<=>\()(.*?)(?=\))", line).group(1)
+            creation_time = re.search(r"^.*\|(\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01]) .*?Z)", line).group(1)
+            findings.append(Finding(rating, full_name, creation_time, None))
+    return findings
+
+if __name__ == '__main__':
+    print(".,:::::: .-:::::'.-:::::' :::      :::.    ::.    :::.  :::::::.. .::::::.:")
+    print(";;;;'''' ;;;'''' ;;;''''  ;;;      ;;`;;   ;;;;,  `;;; ;;;;``;;;;;;;`    ``")
+    print(" [[cccc  [[[,,== [[[,,==  [[[     ,[[ '[[,  [[[[[. '[[  [[[,/[[[''[==/[[[[,")
+    print(" $$      `$$$'`` `$$$'``  $$'    c$$$cc$$$c $$$ 'Y$c$$   $$$$$$c   '''    $")
+    print("888oo,__  888     888   o88oo,.__888   888,888    Y88  888b '88bo,88b    dP")
+    print("M''''YUM   'MM,    'MM,  ''''YUMM YMM   ''` MMM     YMMMMMMM   'W'  'YMmMY'")
+
+    if len(sys.argv) > 1:
+        global filename
+        filename = sys.argv[1]
+        currentPath = os.getcwd()
+
+        global findings
+        print("###############################################")
+        if '.json' in filename:
+            print("Great, looks like you specified a JSON file ...")
+            print("Here we go!")
+            findings = populateSharesfromJSON(filename)
+        elif '.txt' in filename or '.log' in filename:
+            print("Great, looks like you specified a standard output file ...")
+            print("Here we go!")
+            findings = populateSharesfromTXT(filename)
+        else:
+            print("Ok, so you didn't give me a file with an extension (or one I know about)")
+            print("Lets try Snaffler JSON output first!")
+            try:
+                findings = populateSharesfromJSON(filename)
+                print("Yay! That worked!")
+            except:
+                print("!!!!")
+                print("Ok, so it wasn't Snaffler JSON output ...")
+                print("Lets try standard Snaffler output!")
+                try:
+                    findings = populateSharesfromTXT(filename)
+                    if not bool(findings):
+                        raise Exception('...')
+                    print("Yay! That worked!")
+                except:
+                    print("What kind of garbage input is this!!??")
+                    print("Come back when you have Snaffler input to give me.")
+                    sys.exit()
+
+        print("###############################################")
+        webbrowser.open('http://127.0.0.1:5000', new=2)
+        app.run()
+
+    else:
+        print("Please provide a .json or .txt file as a command line argument.")
